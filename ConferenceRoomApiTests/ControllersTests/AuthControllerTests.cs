5745427de9c4b8893c7fd67b8c8d42fd6ccd8cef
José Victor <138397561+zvduainda@users.noreply.github.com>
2025-04-18 13:16:19 -0300

    Tests

--- ConferenceRoomApiTests/ControllersTests/AuthControllerTests.cs
+++ ConferenceRoomApiTests/ControllersTests/AuthControllerTests.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ConferenceRoomApi.Controllers;
+﻿using ConferenceRoomApi.Controllers;
 using ConferenceRoomApi.DTOs.Auth;
 using ConferenceRoomApi.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
